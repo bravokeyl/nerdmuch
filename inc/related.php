782febfb82321803c14f4e39a498546e7c84ccba
bravokeyl <bravokeyl@gmail.com>
2017-03-03 00:04:25 +0530

    Popular posts year cehck

--- inc/related.php
+++ inc/related.php
@@ -1,25 +1,25 @@
-<?php 
+<?php
 /**
  * Science Magazine related posts functions
-**/ 
+**/
 ?>
-<?php 
+<?php
 function related_posts_tags() {
 		wp_reset_postdata();
 	//related posts by tags
 		global $post;
 		$tags = wp_get_post_tags($post->ID);
-		
+
 			if ($tags) {
-				$tag_ids = array();				
-				foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;				
+				$tag_ids = array();
+				foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
 					 $sci1_posts = new WP_Query(array('tag__in' => $tag_ids, 'post__not_in' => array($post->ID), 'posts_per_page'=> 4, 'ignore_sticky_posts' => 1));?>
-	
+
 <div class="blog-category">
-	<ul>			 
-					 
+	<ul>
+
 <?php  while ( $sci1_posts->have_posts()) : $sci1_posts->the_post();?>
-		<li>		
+		<li>
 			<div class="blog-post-image">
 				<?php if (  (function_exists('has_post_thumbnail')) && (has_post_thumbnail())  ) { ?>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
@@ -51,22 +51,22 @@ function related_posts_tags() {
 <?php endwhile;wp_reset_postdata(); ?>
 </ul>
 </div>
-<?php }} 
+<?php }}
 
 function related_posts_category() {
 				wp_reset_postdata();
 			//related posts by category
 				global $post;
 				$categories = get_the_category($post->ID);
-				
+
 					if ($categories) {
 					$category_ids = array();
 					foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
 					 $sci1_rel_posts = new WP_Query(array('category__in' => $category_ids, 'post__not_in' => array($post->ID), 'posts_per_page'=> 4, 'ignore_sticky_posts' => 1));?>
 <div class="blog-category">
-	<ul>			 				 
-<?php  while ( $sci1_rel_posts->have_posts()) : $sci1_rel_posts->the_post();?>											
-		<li>		
+	<ul>
+<?php  while ( $sci1_rel_posts->have_posts()) : $sci1_rel_posts->the_post();?>
+		<li>
 			<div class="blog-post-image">
 				<?php if (  (function_exists('has_post_thumbnail')) && (has_post_thumbnail())  ) { ?>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
@@ -97,8 +97,8 @@ function related_posts_category() {
 		</li><?php endwhile;wp_reset_postdata(); ?>
 </ul>
 </div>
-<?php }} 
- 
+<?php }}
+
 function related_posts_author() {
 				wp_reset_postdata();
 //related posts by author
@@ -106,10 +106,10 @@ function related_posts_author() {
 				$author = get_the_author_meta('ID');
 				$sci1_posts = new WP_Query(array('author' => $author, 'post__not_in' => array( $post->ID ), 'posts_per_page' => 4));?>
 <div class="blog-category">
-	<ul>			 				 
-<?php  while ( $sci1_posts->have_posts()) : $sci1_posts->the_post();?>	
+	<ul>
+<?php  while ( $sci1_posts->have_posts()) : $sci1_posts->the_post();?>
 
-		<li>		
+		<li>
 			<div class="blog-post-image">
 				<?php if (  (function_exists('has_post_thumbnail')) && (has_post_thumbnail())  ) { ?>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
@@ -144,24 +144,26 @@ function related_posts_author() {
 <?php }
 
 function sci1_popular_posts() {
-	
-$popular_post = get_option('sci1_popular_post');	
+
+$popular_post = get_option('sci1_popular_post');
 $category = get_category( get_query_var( 'cat' ) );
 $pop_cat = $category->cat_ID;
 
-if( $popular_post == 'week'){	
+if( $popular_post == 'week'){
 
-					
-	$week = date('W');
+
+	  $week = date('W');
+		$year = date('Y');
 		$args = array(
 			'cat'      => $pop_cat,
 			'posts_per_page'=> '4',
 			'w' => $week,
+			'year'=> $year,
 			'meta_key' => 'post_views_count',
 			'orderby' => 'meta_value_num',
 			'order'    => 'DESC'
 			);
-			
+
 } elseif ($popular_post == 'year'){
 
 	$year = date('Y');
@@ -172,9 +174,9 @@ if( $popular_post == 'week'){
 			'meta_key' => 'post_views_count',
 			'orderby' => 'meta_value_num',
 			'order'    => 'DESC'
-			);	
+			);
 } elseif($popular_post == 'month'){
-	
+
 	$month = date('m');
 		$args = array(
 			'cat'      => $pop_cat,
@@ -185,17 +187,17 @@ if( $popular_post == 'week'){
 			'order'    => 'DESC'
 			);
 }elseif($popular_post == 'forever'){
-	
+
 		$args = array(
 			'cat'      => $pop_cat,
 			'posts_per_page'=> '4',
 			'meta_key' => 'post_views_count',
 			'orderby' => 'meta_value_num',
 			'order'    => 'DESC'
-			);	
+			);
 }
-									
-									
+
+
 $popular_posts = new WP_Query($args); if($popular_posts->have_posts()):?>
 
 
@@ -229,9 +231,9 @@ $popular_posts = new WP_Query($args); if($popular_posts->have_posts()):?>
 <!--slider-container-->
 
 <div class="blog-category popular-slider-control">
-	<ul>	
+	<ul>
 		<?php while($popular_posts->have_posts()): $popular_posts->the_post(); ?>
-		<li>		
+		<li>
 			<div class="blog-post-image">
 				<?php if (  (function_exists('has_post_thumbnail')) && (has_post_thumbnail())  ) { ?>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
